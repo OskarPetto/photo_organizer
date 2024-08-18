0b8dbd52c893c3aa6d8c54840a07962a4019b29c
Oskar Petto <oskar.petto@hotmail.com>
2024-08-18 18:55:11 +0200

    add file_name as date source

--- photo_organizer.py
+++ photo_organizer.py
@@ -2,6 +2,7 @@ import os
 import sys
 from PIL import Image, ExifTags
 from datetime import datetime
+import re
 
 date_keys = (
     'DateTimeOriginal', 'DateTime'
@@ -12,12 +13,16 @@ allowed_extensions = (
 )
 
 def get_photo_date(photo_path):
-    file_date = datetime.utcfromtimestamp(os.path.getmtime(photo_path))
+    fallback_date = datetime.utcfromtimestamp(os.path.getmtime(photo_path))
     try:
+        file_name = os.path.basename(photo_path)
+        if match := re.search("([0-9]{8})", file_name):
+            date_string = match.group(1)
+            fallback_date = datetime.strptime(date_string, '%Y%m%d')
         img = Image.open(photo_path)
         exif_data = img._getexif()
         if exif_data is None:
-            return file_date
+            return fallback_date
         exif_items = { ExifTags.TAGS[k]: v for k, v in exif_data.items() if k in ExifTags.TAGS }
         for date_key in date_keys:
             if date_key in exif_items:
@@ -25,7 +30,7 @@ def get_photo_date(photo_path):
                 return datetime.strptime(date_string, '%Y:%m:%d')
     except Exception as e:
         print(f'Error reading {photo_path}: {e}')
-    return file_date
+    return fallback_date
     
 def organize_photos_by_year(source_dir):
     for root, _, files in os.walk(source_dir):
